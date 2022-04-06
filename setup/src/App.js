8eb4ba638534ae84865511f4de90d8dbf8b19150
Muhammad Asif <asif88383@gmail.com>
2022-04-06 22:07:10 +0500

    initial commit

--- setup/src/App.js
+++ setup/src/App.js
@@ -7,6 +7,22 @@ function App() {
   const [people, setPeople] = useState([data]);
   const [index, setIndex] = useState(0);
 
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+    if(index<0){
+      setIndex(lastIndex);
+    }
+    if(index>lastIndex){
+      setIndex(0);
+    }
+  }, [index, people]);
+
+  useEffect(() => {
+    setInterval(() => {
+      setIndex(index + 1);
+      }, 3000);
+  }, [index]);
+
   return (
     <section className='section'>
       <div className='title'>
@@ -24,7 +40,8 @@ function App() {
           if (personIndex === index) {
             position = 'activeSlide';
           }
-          if (personIndex === index-1 || personIndex === 0) {
+          if (personIndex === index-1 || 
+            (index === 0 && personIndex === people.length-1)) {
             position = 'lastSlide';
           }
           return (
@@ -39,11 +56,11 @@ function App() {
         })}
 
         {/* left right buttons */}
-        <button className='prev'>
-          <FiChevronLeft className='icon' />
+        <button className='prev' onClick={()=>setIndex(index-1)}>
+          <FiChevronLeft/>
         </button>
-        <button className='next'>
-          <FiChevronRight className='icon' />
+        <button className='next' onClick={()=>setIndex(index+1)}>
+          <FiChevronRight/>
         </button>
       </div>
     </section>
